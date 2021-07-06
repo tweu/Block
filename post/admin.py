09ac435344f1f91c8069da4769e084a24c14be67
Maxim <tweuuuuu@gmail.com>
2021-07-06 22:05:51 +0600

    set MEDIA_ROOT , change model Post, configurate admin

--- post/admin.py
+++ post/admin.py
@@ -1,5 +1,36 @@
 from django.contrib import admin
+from django.contrib.admin.decorators import register
+from post.models import Category, Post, Tag
+from django.utils.safestring import mark_safe
 
 # Register your models here.
 
-# admin.site.register()
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title')
+    prepopulated_fields ={'slug': ('title', )}
+
+# admin.site.register(Tag, TagAdmin)
+
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title')
+    prepopulated_fields = {'slug': ('title', )}
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'author', 'created_at', 'views', 'get_photo')
+    prepopulated_fields= {'slug': ('title', )}
+    fields = ('title', 'slug', 'author', 'category', 'content', 'tags', 'views', 'photo', 'get_photo')
+    list_filter = ('author', 'category', 'created_at')
+    readonly_fields = ('views', 'get_photo' )
+
+    def get_photo(self, obj):
+        if obj.photo:
+            return mark_safe(f'<img src="{obj.photo.url}" width="50">')
+        return '-'
+
+    get_photo.short_description = 'Фото'
--- post/models.py
+++ post/models.py
@@ -36,14 +36,15 @@ class Post(models.Model):
     title = models.CharField(max_length=250, verbose_name='Заголовок')
     content = models.TextField(verbose_name='Контент')
     author = models.ForeignKey(
-        settings.AUTH_USER_MODEL, on_delete=PROTECT, verbose_name='Автор')
+        settings.AUTH_USER_MODEL, on_delete=PROTECT, verbose_name='Автор', related_name= 'posts')
     category = models.ForeignKey(
-        Category, on_delete=CASCADE, verbose_name='Категория')
+        Category, on_delete=CASCADE, verbose_name='Категория', related_name='posts')
     tags = models.ManyToManyField(Tag, verbose_name='Тэги')
     created_at = models.DateTimeField(
-        auto_now_add=True, verbose_name='Дата создания')
+        auto_now_add=True, verbose_name='Опубликовано')
     slug = models.SlugField(verbose_name='Слаг')
-    views = models.PositiveIntegerField(default=0)
+    views = models.PositiveIntegerField(default=0, verbose_name= 'Количество просмотров')
+    photo = models.ImageField(upload_to = 'posts/%Y/%m/%d/', verbose_name='Фото', null = True, blank = True)
 
     def __str__(self):
         return self.title
